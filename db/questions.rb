aec95f38175e8991d2fa5b76cdbe87daa562331c
Austin Winslow <awinslow21@gmail.com>
2013-12-12 17:12:48 -0500

    Finished ActiveRecord problem set

--- db/questions.rb
+++ db/questions.rb
@@ -1,10 +1,14 @@
 #What 3 towns have the highest population of citizens that are 65 years and older?
-
+TownHealthRecord.select(:geography, :over_65_2005).where("geography != 'Massachusetts Total'").order("over_65_2005 DESC").limit(3)
 
 #What 3 towns have the highest population of citizens that are 19 years and younger?
+TownHealthRecord.select(:geography, :ages_0to19_2005).where("geography != 'Massachusetts Total'").order("ages_0to19_2005 DESC").limit(3)
 
 #What 5 towns have the lowest per capita income?
+TownHealthRecord.select(:geography, :income_per_capita_2000).where("geography != 'Massachusetts Total'").order("income_per_capita_2000").limit(5)
 
 #Omitting Boston, Becket, and Beverly, what town has the highest percentage of teen births?
+TownHealthRecord.select(:geography, :percent_teen_births_2005_2008).where("geography != 'Massachusetts Total' AND geography != 'Boston' AND geography != 'Becket' AND geography != 'Beverly'").order("percent_teen_births_2005_2008 DESC").limit(1)
 
 #Omitting Boston, what town has the highest number of infant mortalities?
+TownHealthRecord.select(:geography, :infant_mortality_rate_per_1000_births_2005_2008).where("geography != 'Boston' AND geography != 'Massachusetts Total'").order("infant_mortality_rate_per_1000_births_2005_2008 DESC").limit(1)
